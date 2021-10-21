a11c2ebfd06b0a16667ce4eb48b72abd2ab70841
antonioayala <antonioayala@localhost>
2021-10-21 09:04:35 -0500

    Prueba

---
+++ POOPrueba/src/main/java/com/mycompany/pooprueba/Prueba.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pooprueba;
+
+/**
+ *
+ * @author antonioayala
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("MAin");
+    }
+}
